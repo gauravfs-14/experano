bc58bdf90b2eecd0b72ff01ffb1563fcb7f0ffd3
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2025-02-28 21:06:53 -0600

    feat: user auth

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/navbar/default";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
